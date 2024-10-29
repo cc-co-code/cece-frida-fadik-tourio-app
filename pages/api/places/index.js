631b7fd57db9957a82064e2e3393de431e8f3969
YOUR NAME <cecilia@bartlewski.de>
2024-10-29 15:28:31 +0100

    read done

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,15 @@
-import { db_places } from "../../../lib/db_places";
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/places";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
+export default async function handler(request, response) {
+  await dbConnect();
+  if (request.method === "GET") {
+    const place = await Place.find();
+
+    console.log(place);
+
+    return response.status(200).json(place);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
